ebca1037f46d3985d01b01dcf91f962145f721ed
KING AB <louissaintjulens1@gmail.com>
2026-09-20 21:04:48 -0400

    Adicionar novo produto

--- products.py
+++ products.py
@@ -22,5 +22,35 @@ products = [
         "price": "R$ 29,98",
         "image": "/static/produto4.jpg",
         "link": "https://s.shopee.com.br/8Kpfk3BKGA"
-    }
+    },
+    {
+        "name": "Gel Adesivo para vaso sanitário Pastilha adesiva Florzinha para vaso Cheirinho banheiro - Casa Limpa",
+        "price": "R$ 13,99",
+        "image": "/static/produto5.jpg",
+        "link":"https://s.shopee.com.br/1AXwz3LjL"
+    },
+    {
+        "name": "Capa De Chuva Moto Motoqueiro Impermeável Reforçada",
+        "price": "R$ 41,99",
+        "image": "/static/produto6.jpg",
+        "link":"https://s.shopee.com.br/8KpftxnDO4",
+    },
+    {
+        "name": "Capa De Chuva + Bota Motoqueiro Reforçada Pvc Resistente Original",
+        "price": "R$ 44,90",
+        "image": "/static/produto7.jpg",
+        "link":"https://s.shopee.com.br/AKakI1VuFf",
+    },
+    {
+        "name": "Espanador Elétrico Pena Escova De Limpeza Ajustável Escova De Limpeza Doméstica Vassoura Elétrica",
+        "price": "R$ 69,99",
+        "image": "/static/produto8.jpg",
+        "link":"https://s.shopee.com.br/6fhRvkY0uk"
+    },
+    {
+        "name": "Escova de Limpeza 9 Em 1 Recarregavél Elétrica Giratória Com Cabo Alongador-D10",
+        "price": "R$ 79,99",
+        "image": "/static/produto9.jpg",
+        "link":"https://s.shopee.com.br/gQEn19aAd",
+    },
 ]
